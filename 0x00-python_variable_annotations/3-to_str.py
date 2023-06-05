9f2d6ad33f0a48034b7bdbe620c411677df86344
yonil1-hub <wyml@yahoo.com>
2023-06-05 09:24:13 +0300

    string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-This module defines a function for converting a float to its string representation.
+This module defines a function for converting
+a float to its string representation.
 """
 
 
